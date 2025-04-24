62eb4f1a6fc9347ce766e240215a903cb3bc7f93
labuser <rajat.sahoo@in.ey.com>
2025-04-24 21:21:23 +0000

    commit

--- src/main/java/com/example/ey/gateway/GatewayApplication.java
+++ src/main/java/com/example/ey/gateway/GatewayApplication.java
@@ -2,27 +2,17 @@ package com.example.ey.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/notification-service/**")
-						.uri("http://localhost:9000/"))
-				.route(p -> p
-						.path("/customer-service/**")
-						.uri("http://localhost:9001/"))
-				.build();
 	}
 
 }
